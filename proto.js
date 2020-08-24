dcb48116e83d692299c9ea79b25506f468e439b1
Jacob Harrington <jharrington4@gmail.com>
2020-08-24 10:41:45 -0700

    extracted parser into function that takes userID

--- proto.js
+++ proto.js
@@ -2,22 +2,26 @@
 const fs = require('fs').promises;
 const {  parseBinaryToJson, parseDataForResponse } = require('./utils/parseBuffer');
 
-fs.readFile('./txnlog.dat')
-  .then(buf => parseBinaryToJson(buf))
-  .then(parsedData => {
-    const userData = parsedData.filter(item => item.userId === '2456938384156277127');
+const protoParser = userID => {
+  return fs.readFile('./txnlog.dat')
+    .then(buf => parseBinaryToJson(buf))
+    .then(parsedData => {
+      const userData = parsedData.filter(item => item.userId === userID);
 
-    return {
-      globalData: parsedData,
-      userData
-    };
-  })
-  .then(({ globalData, userData }) => parseDataForResponse(globalData, userData))
-  .then(parsedResponse => (`
+      return {
+        globalData: parsedData,
+        userData
+      };
+    })
+    .then(({ globalData, userData }) => parseDataForResponse(globalData, userData))
+    .then(parsedResponse => (`
 total credit amount=${parsedResponse.creditAmount}
 total debit amount=${parsedResponse.debitAmount}
 autopays started=${parsedResponse.autoPayStarted}
 autopays ended=${parsedResponse.autoPayEnded}
 balance for user ${parsedResponse.userID}=${parsedResponse.balance}`))
-  .then(console.log)
-  .catch(console.log);
+    .then(console.log)
+    .catch(console.log);
+};
+
+protoParser('2456938384156277127');
